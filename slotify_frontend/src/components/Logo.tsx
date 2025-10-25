f977a0e12f36e298736159a5fed378394ee12406
AMBESH RATURI <117914021+AmbeshRaturiCoupa@users.noreply.github.com>
2025-10-26 00:08:34 +0530

    Fix navbar alignment

--- slotify_frontend/src/components/Logo.tsx
+++ slotify_frontend/src/components/Logo.tsx
@@ -1,12 +1,12 @@
 import slot from '../assets/Adobe_Express_-_file.png';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
     <Link to="/">
-        <img src={slot} width="50" height="50"></img>
+      <img src={slot} className="h-10 w-10"></img>
     </Link>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
