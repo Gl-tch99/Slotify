3b188164f8bfbbd16f206e0ea0a1d3347cf82df5
Gl-tch99 <107829679+Gl-tch99@users.noreply.github.com>
2025-10-28 00:18:45 +0530

    Navbar fixed and site colors added (#14)

--- slotify_frontend/src/components/Logo.tsx
+++ slotify_frontend/src/components/Logo.tsx
@@ -1,10 +1,14 @@
 import slot from '../assets/Adobe_Express_-_file.png';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Logo = () => {
   return (
     <Link to="/">
-      <img src={slot} className="h-10 w-10"></img>
+      <div className="flex top-5 justify-center items-center">
+        <HomeIcon className="m-2" />
+      </div>
+      {/* <img src={slot} className="h-10 w-10"></img> */}
     </Link>
   );
 };
