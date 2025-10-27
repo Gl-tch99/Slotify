aa5d213676f615404271a46ffddedd88b99d2f82
Gl-tch99 <107829679+Gl-tch99@users.noreply.github.com>
2025-10-28 00:18:45 +0530

    Navbar fixed and site colors added (#14)

--- slotify_frontend/src/components/Navbar.tsx
+++ slotify_frontend/src/components/Navbar.tsx
@@ -1,35 +1,30 @@
 import { Link } from 'react-router-dom';
 import ToggleTheme from './ToggleTheme';
 import Logo from './Logo';
-import SearchBox from './SearchBox';
-import wave from '../assets/wave.svg';
+import SetLocation from './SetLocation';
 
 const Navbar = () => {
   return (
     <>
-      <div>
-        <nav className="flex items-center fixed top-5 right-0 left-0 bg-white dark:bg-gray-900 text-black dark:text-amber-50 bg-opacity-20 py-3 px-7 border rounded-2xl bottom-8 gap-7 backdrop-blur-lg w-vh h-max">
-          <div className="flex flex-1/2">
-            <div>
-              <Logo />
-            </div>
-            <div className="w-full flex">
-              <SearchBox entity="location" width={96} />
-            </div>
+      <nav className="sticky flex h-max w-screen items-center top-0 left-0 right-0 bg-light-accent dark:bg-dark-accent text-black dark:text-amber-50 bg-opacity-20 py-3 px-7 bottom-8 gap-7 backdrop-blur-lg">
+        <div className="flex flex-1/2 gap-5 text-light-ui dark:text-dark-ui">
+          <Logo />
+          <div className="w-full flex">
+            <SetLocation />
           </div>
-          <div className="flex-1/2 flex justify-end">
-            <div className="px-3" id="projects">
-              <Link to="/book_venue">Book Venue</Link>
-            </div>
-            {/* <div className="flex-1/4" id="friends">
-            <img src={wave}></img>
-            </div> */}
-            <div className="" id="projects">
-              <ToggleTheme />
-            </div>
+        </div>
+        <div className="flex-1/2 flex justify-end gap-5 text-light-ui dark:text-dark-ui">
+          <div id="projects">
+            <Link to="/book_venue">Book Venue</Link>
           </div>
-        </nav>
-      </div>
+          <div className="" id="projects">
+            <ToggleTheme />
+          </div>
+          <div id="projects">
+            <Link to="/login">Login/Sign-up</Link>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
