f5c8e6b095b6133dfedc6f94b208df7aab39e027
Gl-tch99 <107829679+Gl-tch99@users.noreply.github.com>
2026-04-08 20:45:29 +0530

    added_venue_opertaing_time_controller (#12)

--- slotify_backend/app/models/venue.rb
+++ slotify_backend/app/models/venue.rb
@@ -3,6 +3,7 @@ class Venue < ApplicationRecord
   has_many :venue_operating_times, dependent: :destroy
   belongs_to :address
   accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :venue_operating_times
   belongs_to :user, foreign_key: :owner_user_id
   
   has_and_belongs_to_many :sports
--- slotify_backend/app/models/venue_operating_time.rb
+++ slotify_backend/app/models/venue_operating_time.rb
@@ -1,3 +1,5 @@
 class VenueOperatingTime < ApplicationRecord
     belongs_to :venue
+    validates :opening_time, presence: true
+    validates :closing_time, presence: true
 end
